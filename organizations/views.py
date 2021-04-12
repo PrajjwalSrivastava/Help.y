b91a43e164fa43081a7f3528ee85829741db9046
Prajjwal <prajju.s1999@gmail.com>
2021-04-12 13:11:27 +0530

    Added auth and deleted model

--- organizations/views.py
+++ organizations/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
 def home(request):
     return render(request,'organizations/home.html')
+
+@login_required
+def create(request):
+    return render(request, 'organizations/create.html')
